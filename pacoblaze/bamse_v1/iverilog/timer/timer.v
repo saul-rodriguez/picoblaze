a2d4f155813e164e6197675c35de97687180b4cb
saul <saul@example.com>
2019-10-03 15:19:38 +0200

    timer bamse created

--- pacoblaze/bamse_v1/iverilog/timer/timer.v
+++ pacoblaze/bamse_v1/iverilog/timer/timer.v
@@ -9,10 +9,9 @@ module timer(
 	en,
 	go,
 	auto_load,
+	tmr_int_clear,	
 	tmr_int,
-	go_clear,
-	address,
-	ren,	
+	go_clear	
 );
 
 input clk_in;
@@ -22,12 +21,9 @@ input [15:0] timer_conf; // 16 bit load value for the counter
 input en; //enable (H)
 input go; //start (H)
 input auto_load; //automatic restart timer after rolloff (H)
+input tmr_int_clear; 
 output reg tmr_int; //interrupt (H)
 output reg go_clear;
-input [7:0] address;
-input ren;
-
-parameter ADDR = 8'b0000_0000; 
 
 reg [6:0] prescaler_out;
 
@@ -71,13 +67,8 @@ reg [1:0] timer_state;
 
 parameter IDLE  = 2'b00, 
 		  GO =    2'b01,
-		  ROLL  = 2'b11,
-		  WAIT  = 2'b10;
-
-
-wire read_flag;
-assign read_flag = (address == ADDR && ren)? 1'b1 : 1'b0;
-
+		  ROLL  = 2'b11;
+		 
 always @(posedge selected_freq) begin
 	if (rst) begin
 		timer_count <= 0;					
@@ -95,7 +86,7 @@ always @(posedge selected_freq) begin
 								prescaler_reset <= 1;
 							end 
 							
-							if (read_flag) tmr_int <= 0;
+							if (tmr_int_clear) tmr_int <= 0;
 						
 						end
 				GO:		begin 
@@ -107,7 +98,7 @@ always @(posedge selected_freq) begin
 								go_clear <= 1;
 							end							
 							
-							if (read_flag) tmr_int <= 0;													
+							if (tmr_int_clear) tmr_int <= 0;													
 						end
 				ROLL: 	begin
 							tmr_int <= 1;
@@ -126,6 +117,81 @@ always @(posedge selected_freq) begin
 	
 	end
 end
+
+endmodule
+
+/**************************/
+/*    TIMER BAMSE         */
+/**************************/
+
+// Configuration word
+//      B7       B6        B5       B4        B3        B2        B1        B0
+//   	-        PS2      PS1       PS0      AUTO_LD    EN        GO      INT_TMR
+
+module TIMER_BAMSE (
+	input clk,
+	input rst, //(H)
+	input [15:0] timer_conf, // 16 bit load value for the counter 
+	//input en, //enable (H)
+	output tmr_interrupt, //interrupt (H)
+	input [7:0] address,
+	input [7:0] config_in,
+	output [7:0] config_out,
+	input ren,
+	input wen	
+);
+
+parameter ADDR = 8'h00; // This same register address is used for read and write
+
+reg  [7:0] timer_config_reg;
+wire [1:0] mask_reset;
+reg tmr_int_clear;
+wire go_clear;
+wire tmr_int;
+
+assign mask_reset[0] = tmr_int;
+assign mask_reset[1] = go_clear;
+
+always @(posedge clk) begin
+	if (rst) begin
+		timer_config_reg <= 0;
+		tmr_int_clear <= 0;
+	end else if (|mask_reset) begin // Signals from timer module 
+			if (mask_reset[0]) timer_config_reg[0] <= 1; // To interrupt out
+			if (mask_reset[1]) timer_config_reg[1] <= 0; // To GO
+		end	else if (address == ADDR) begin //signals written from software
+		if (wen) begin
+			timer_config_reg <= config_in;
+			if (config_in[0] == 0) tmr_int_clear <= 1;			
+		end		
+		if (tmr_int_clear) tmr_int_clear <= 0;
+	end
+end
+
+assign config_out = timer_config_reg;
+
+wire go, en, auto_load;
+wire [2:0] prescaler_conf;
+
+assign tmr_interrupt = config_out[0];
+assign go = config_out[1];
+assign en = config_out[2];
+assign auto_load = config_out[3];
+assign prescaler_conf[2:0] = config_out[6:4];
+
+timer tmt(
+	.clk_in(clk),
+	.rst(rst),
+	.prescaler_conf(prescaler_conf),
+	.timer_conf(timer_conf),
+	.en(en),
+	.go(go),
+	.auto_load(auto_load),
+	.tmr_int_clear(tmr_int_clear),	
+	.tmr_int(tmr_int),
+	.go_clear(go_clear)
+);
+
 
 endmodule
 
--- pacoblaze/bamse_v1/iverilog/timer/timer_tb.v
+++ pacoblaze/bamse_v1/iverilog/timer/timer_tb.v
@@ -6,24 +6,23 @@
 
 module timer_tb();
 
-reg clk_in;
+reg clk;
 reg rst;
 reg [2:0] prescaler_conf;
 reg [15:0] timer_conf; 
 reg en; //enable (H)
 reg go; //start (H)
 reg auto_load; //automatic restart timer after rolloff (H)
-wire tmr_int; //interrupt (H)
-wire go_clear;
+wire tmr_interrupt; //interrupt (H)
 reg [7:0] address;
+wire [7:0] config_in;
+wire [7:0] config_out;
 reg ren;
 reg wen;
 
-
-reg go_aux;
-
-parameter ADDR=8'h00
-
+assign config_in = {{1'b0},prescaler_conf,auto_load,en,go,tmr_interrupt};
+parameter ADDR=8'h00;
+/*
 //Resetable register
 always @(posedge clk_in) begin
 	if (rst) begin
@@ -34,29 +33,28 @@ always @(posedge clk_in) begin
 		go_aux <= go;	
 	end
 end
+*/
 
-
-timer #(.ADDR(8'h00)) dut(
-	.clk_in(clk_in),
-	.rst(rst),
-	.prescaler_conf(prescaler_conf),
+TIMER_BAMSE #(.ADDR(8'h00)) dut(
+	.clk(clk),
+	.rst(rst),	
 	.timer_conf(timer_conf),
-	.en(en),
-	.go(go_aux),
-	.auto_load(auto_load),
-	.tmr_int(tmr_int),
+	//.en(en),		
+	.tmr_interrupt(tmr_interrupt),
 	.address(address),
+	.config_in(config_in),
+	.config_out(config_out),
 	.ren(ren),
-	.go_clear(go_clear)
+	.wen(wen)
 );
 
-always #(2) clk_in = ~clk_in;
+always #(2) clk = ~clk;
 
 initial begin
 	$dumpfile("dump.vcd");
-	$dumpvars(0,dut,go,go_aux);
+	$dumpvars(0,dut,config_in,config_out);
 	
-	clk_in = 0;
+	clk = 0;
 	rst = 0;
 	prescaler_conf = 0;
 	timer_conf = 16'hfff0;
@@ -65,14 +63,19 @@ initial begin
 	auto_load = 0;
 	address = 8'h00;
 	ren = 0;
+	wen = 0;
+		
 	
 	#10 rst = 1;
 	#10 rst = 0;
+	
 	#50 en = 1;
 	#10 go = 1;
-	#10 go = 0;
+	#10 wen = 1;
+	#10 go = 0; wen = 0;
 	
-		
+	
+		/*
 	#100 rst = 1;
 	#10 rst = 0;
 	#10 auto_load = 1;
@@ -85,7 +88,7 @@ initial begin
 	#100 ren = 1;
 	#10 ren = 0;
 	
-	#200 
+	#200 */
 	#(250) $finish;
 end
 
